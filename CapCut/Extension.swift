2cbddfb7145e57cd2c696e85c5ccf4255e036066
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-10-09 14:58:47 +0100

    added

--- CapCut/Extension.swift
+++ CapCut/Extension.swift
@@ -34,3 +34,16 @@ struct DatePickerView: View {
         }
     }
 }
+
+extension String {
+    var isValidEmail: Bool {
+        NSPredicate(format: "SELF MATCHES %@", "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}").evaluate(with: self)
+    }
+}
+
+extension String {
+    var isValidPassword: Bool {
+        let passwordRegex = "^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*()\\-_=+{}|?>.<,:;~`’]{8,}$"
+        return NSPredicate(format: "SELF MATCHES %@", passwordRegex).evaluate(with: self)
+    }
+}
