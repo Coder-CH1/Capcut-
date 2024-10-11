88d6414b234b2a2bfe64916e5d68b15955bd3bbd
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-10-11 14:29:42 +0100

    added

--- CapCut/UserViewModel.swift
+++ CapCut/UserViewModel.swift
@@ -11,7 +11,13 @@ import NIO
 import SwiftUI
 
 class UserViewModel: ObservableObject {
-    let client = Client()
+    
+    let endpoint: String?
+    let projectId: String?
+    let apiKey: String?
+    let client: Client
+    let account: Account
+    
     @Published var email = ""
     @Published var password = ""
     @Published var dateOfBirth = ""
@@ -20,11 +26,27 @@ class UserViewModel: ObservableObject {
     @Published var errorMessage = ""
     @Published var otpSent: Bool = false
     
-    let endpoint = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_ENDPOINT") as? String
-    let projectId = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_PROJECT_ID") as? String
-    let apiKey = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_API_KEY") as? String
+    init() {
+        self.endpoint = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_ENDPOINT") as? String
+        self.projectId = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_PROJECT_ID") as? String
+        self.apiKey = Bundle.main.object(forInfoDictionaryKey: "APPWRITE_API_KEY") as? String
+        self.client = Client()
+            .setEndpoint(endpoint ?? "")
+            .setProject(projectId ?? "")
+            .setSelfSigned()
+        self.account = Account(client)
+    }
     
-    func register() {}
+    func register() async {
+        do {
+           let user = try await account.create(userId: "", email: email, password: password)
+            self.errorMessage = "User registered \(user.email)"
+        } catch let error as AppwriteError {
+            self.errorMessage = error.message
+        } catch {
+            self.errorMessage = "Error occured."
+        }
+    }
     func login() {}
     func logout() {}
 }
